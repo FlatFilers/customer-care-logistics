6e3377d13c11b5de236662f22acd2e65a8c36d35
Jeffrey Fiddler <jeff@flatfile.io>
2025-02-25 09:50:21 -0800

    init

---
+++ src/flatfile/blueprints/documents/welcome.to.flatfile.ts
@@ -0,0 +1,8 @@
+import type { Flatfile } from "@flatfile/api";
+
+export const blueprint: Flatfile.DocumentConfig = {
+  title: "Welcome to Flatfile! 🚀",
+  body: `# Hello, world!`,
+};
+
+export default blueprint;
